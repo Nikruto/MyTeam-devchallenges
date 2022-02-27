f3aa817e3feab13f78076aad4d2e7c7fd1bb0d9c
Nikruto <nikruto1456@gmail.com>
2022-02-27 15:44:30 +0300

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+404 NOT FOUND - @devchallenges.io
+
+[Live](https://evanescent-shame.surge.sh)\
+[Challenge](https://devchallenges.io/challenges/hhmesazsqgKXrTkYkt0U)
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      sans: "Poppins",
+      logo: "PT Serif",
+    },
+  },
+  plugins: [],
+};
